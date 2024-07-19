a8c673e3a6c2ae369b19ce55a871dc8c678f3b6d
sanghyuck-yoon <tkdgur4848@gmail.com>
2024-07-20 00:58:36 +0900

    구글서치개발

--- namu_crawler.py
+++ namu_crawler.py
@@ -1,21 +1,23 @@
+# pylint: disable=trailing-whitespace, line-too-long
+"""Namuwiki crawler class"""
+import re
 import requests
 from bs4 import BeautifulSoup
-import re
 
-class NamuCrawler(object):
+class NamuCrawler():
+    """나무 위키 문서 크롤러 클래스"""
     def __init__(self, url, hop):
         self.url = url
         self.hop = hop # 현재 문서가 몇번째 hop(depth)으로 크롤링 되고 있는지
         self.session = requests.Session()
         html_doc = self.session.get(url)
         self.soup = BeautifulSoup(html_doc.text, 'html.parser')
+        self.toc_dict = {}
 
     def construct_toc(self):
         """## 목차(TOC) 정보 추출 및 TOC 딕셔너리 구성"""
         toc = self.soup.find("div", class_ = 'toc-indent')
-        self.toc_dict = dict()
 
-        """만약 프로필이 있는 문서라면 가장 앞에 구성"""
         html_str = str(self.soup)
         start_pos = html_str.find(str('<body>')) + len(str('<body>'))
         toc_str = str(self.soup.find("div", class_='wiki-macro-toc'))
@@ -23,53 +25,91 @@ class NamuCrawler(object):
         between_content = html_str[start_pos:end_pos]
         soup_between = BeautifulSoup(between_content, 'html.parser')
 
+
+        #만약 프로필이 있는 문서라면 가장 앞에 구성
         if len(profile := soup_between.find_all('div', class_ = 'wiki-table-wrap table-right')) > 0:
             self.toc_dict['s-p'] = (("0.",'PROFILE'), profile[-1]) # dictionary의 첫번째 아이템으로 넣기
 
-        
-        """목차 아이템들을 하나씩 딕셔너리에 저장(key = s-#.#.#, value = (목차 명, 목차 element))"""
+        #목차 아이템들을 하나씩 딕셔너리에 저장(key = s-#.#.#, value = (목차 명, 목차 element))
         pattern = r'^(\d+\.)+' # 제목 파싱용 패턴
 
-        for i, e in enumerate(toc.find_all("span", class_  = "toc-item")):
-            item_value = e.get_text()
+        for ele in toc.find_all("span", class_  = "toc-item"):
+            item_value = ele.get_text()
             numbers = re.match(pattern, item_value).group()
             text = re.sub(pattern, '', item_value).strip()
-            self.toc_dict[e.find('a')['href'].replace("#", "")] = ((numbers, text), self.soup.find('a', id=e.find('a')['href'].replace("#", "")))
+            self.toc_dict[ele.find('a')['href'].replace("#", "")] = (
+                (numbers, text), self.soup.find('a', id=ele.find('a')['href'].replace("#", ""))
+            )
 
-        """마지막 원소로 각주영역 저장, 없다면 None 저장"""
-        self.toc_dict['s-f'] = (("EOD.", 'FOOTNOTES'), self.soup.find("div", class_ = 'wiki-macro-footnote')) # 마지막엔 각주 영역
+        #마지막 원소로 각주영역 저장, 없다면 None 저장
+        self.toc_dict['s-f'] = (# 마지막엔 각주 영역
+            ("EOD.", 'FOOTNOTES'), self.soup.find("div", class_ = 'wiki-macro-footnote')
+        ) 
 
-    def get_doc_title(self):
+    def get_doc_title(self) -> str:
+        """URL에서 현재 문서의 타이틀(주제) 반환 """
         return self.soup.find("a", href = "/w/"+self.url.split("/w/")[-1].split("?")[0]).get_text()
 
     def print_toc(self):
-        """목차 프린트용 메서드"""
+        """포맷화된 목차 프린트 메서드"""
         print(f"\nDocument of {self.get_doc_title()}\n")
         print("\n================== Table of Contents  ==================\n")
-        for k, v in self.toc_dict.items():
-            num = v[0][0].count(".")
+        for val in self.toc_dict.values():
+            num = val[0][0].count(".")
+            index = val[0][0]
+            toc_item = val[0][1]
             if num == 0:
-                print(v[0][0] + " " + v[0][1])
+                print(index + " " + toc_item)
             else:
-                print("".join(["\t"] * (num - 1)) + v[0][0] + " " + v[0][1])
+                print("".join(["\t"] * (num - 1)) + index + " " + toc_item)
 
-    def strip_footnotes(slef, ele):
+    def get_ancestor_items(self, toc_index, level = None):
+        """하나의 목차 item에 대한 상위 목차들을 모두 찾아 반환(현재 페이지 한정))"""
+        toc_index = toc_index[:-1] if toc_index [-1] == "." else toc_index
+        if level is None:
+            level = len(toc_index.split(".")) - 1
+            
+        cur_toc_index = toc_index
+        ancestors = []
+        for i in range(level):
+            ancestors.insert(
+                0, self.toc_dict.get(f"s-{'.'.join(cur_toc_index.split(sep = '.')[:-1])}")[0]
+            )
+            cur_toc_index = ancestors[-1][0][:-1]  
+        toc_items = "/".join([i[1] for i in ancestors])
+        return toc_items
+    
+    def strip_footnotes(self, ele) -> str:
         """각주를 벗겨 텍스트에 삽입하는 함수"""
         content_list = []
         prv_tag = None
-        for c in ele.descendants:
-            if c.name == 'a' and 'wiki-fn-content' in c.get('class', []):
+        for child in ele.descendants:
+            if child.name == 'a' and 'wiki-fn-content' in child.get('class', []):
                 # 'wiki-fn-content' 클래스를 가진 'a' 태그를 리스트에 추가
-                content_list.append(f"({c['href']}; {c['title']})")
+                content_list.append(f"({child['href']}; {child['title']})")
                 prv_tag = True
-            elif isinstance(c, str):
+            elif isinstance(child, str):
                 # 텍스트 노드를 리스트에 추가
-                if prv_tag == True:
+                if prv_tag is True:
                     prv_tag = False
                     continue
-                content_list.append(c.string.strip())
+                content_list.append(child.string.strip())
                 prv_tag = False
         return "".join(content_list)
+    
+    def strip_table(self, ele) -> str:
+        """테이블을 comma-seperated 스타일로 변환하여 반환"""
+        content_list = []
+        for content in ele.find_all("div", class_ = 'wiki-paragraph'):
+            if content.find("a", class_ = 'wiki-fn-content') is not None: ## 각주가 있을 경우
+                content_list.append(self.strip_footnotes(content))
+            else:
+                content_list.append(content.get_text() + ",")
+        return  "".join(content_list)
+    
+    def strip_ex_links(self, ele) -> str:
+        """링크를 벗겨 텍스트에 삽입하는 함수, 추가 개발 필요"""
+        return ele.get_text()
 
     def get_content_between_tags(self, head, start_tag, end_tag):
         """두개의 태그 사이의 wiki-paragraph 정보 추출"""
@@ -77,37 +117,57 @@ class NamuCrawler(object):
 
         # 시작 태그와 끝 태그의 위치를 찾아 사이의 컨텐츠를 추출, 임시 soup로 만듦
         start_pos = html_str.find(str(start_tag))
-        end_pos = html_str.find(str(end_tag))
+        if head[1] == "PROFILE": ## 프로필 이라면 목차 전까지
+            end_pos = html_str.find(str('<div class="wiki-macro-toc"'))
+        else:
+            end_pos = html_str.find(str(end_tag))
         between_content = html_str[start_pos:end_pos]
         soup_between = BeautifulSoup(between_content, 'html.parser')
 
-        # wiki-paragraph를 가진 엘리먼트를 수집, 텍스트 컨텐츠 추출
-        elements_between = soup_between.find_all('div', class_='wiki-paragraph')
+        # 헤더가 PROFILE인 경우: 첫번째 테이블만 가져오기
+        if head[1] == "PROFILE":
+            content = self.strip_table(soup_between.find("table"))
+            return (head, [content])
+        # PROFILE이 아닌 경우: 추출할 원소들 리스트에 부모의 태그가 td가 아닌 경우 wiki-paragraph를 가진 엘리먼트를 수집, 거기에 wiki-table까지 추가로 넣기
+        elements_between = [ele for ele in soup_between.find_all('div', class_='wiki-paragraph') if ele.find_parent().name != "td"]
+        elements_between += list(soup_between.find_all('table', class_ = 'wiki-table'))
 
-        if len(elements_between) == 0:
+        if len(elements_between) == 0 or (len(elements_between) == 1 and elements_between[0].get_text() == ""):
             # 설명이 아예 없는 경우 아래 안내 메세지 반환
-            return (head, "해당 섹션에 대한 설명이 없거나 하위 문서로 대체됩니다.")
-        elif (ext_icon := elements_between[0].find("img", alt = '상세 내용 아이콘')) != None:
+            return (head, ["해당 섹션에 대한 설명이 없거나 하위 문서로 대체됩니다."])
+        if (elements_between[0].find("img", alt = '상세 내용 아이콘')) is not None:
             # 타 문서로 설명이 대체된 경우엔 링크 반환
             ext_link = elements_between[0].find("a", class_ = "wiki-link-internal")['href']
             return (head, ext_link)
-        else: 
-            # 설명이 있는 경우엔 get_text()로 텍스트 반환
-            text_content = []
-            for element in elements_between:
-                # 만약 펼치기/접기 버튼이 있으면 그냥 넘어가기 : 데이터가 중복임
-                if element.find("dl", class_ = 'wiki-folding') != None:
-                    continue
-                # 만약 각주가 있는 엘리먼트라면 각주를 strip하는 함수 적용
-                elif element.find("a", class_ = 'wiki-fn-content') != None:
-                    text_content.append(self.strip_footnotes(element))
-                #아니면 그냥 일반 get_text() 적용
-                else:
-                    text_content.append(element.get_text())
-            return (head, text_content)
+         
+        # 설명이 있는 경우엔 get_text()로 텍스트 반환
+        text_content = []
+        for element in elements_between:
+            # 만약 펼치기/접기 버튼이 있으면 그냥 넘어가기 : 데이터가 중복임
+            if element.find("dl", class_ = 'wiki-folding') is not None:
+                continue
+            
+            # 만약 테이블 요소를 가지고 있거나 자체가 테이블 클래스라면 테이블을 strip하는 함수 적용
+            if element.find("table", class_ = 'wiki-table') is not None or element.get('class')[0] == 'wiki-table':
+                text_content.append(self.strip_table(element))
+
+            # 만약 각주가 있는 엘리먼트라면 각주를 strip하는 함수 적용
+            elif element.find("a", class_ = 'wiki-fn-content') is not None:
+                text_content.append(self.strip_footnotes(element))
+            
+            # 외부 링크가 있으면 링크를 strip 하는 함수 적용 => 추가 개발 필요
+            # elif element.find("a", class_ = 'wiki-link-external') is not None:
+            #     text_content.append(self.strip_ex_links(element))
+
+            
+
+            else: #아니면 그냥 일반 get_text() 적용
+                text_content.append(element.get_text())
+                
+        return (head, text_content)
         
 
-    def get_item_and_next(self, target_key):
+    def get_next_item(self, target_key):
         """toc_dict에서 현재 헤드의 다음 헤드 값을 반환"""
         keys = list(self.toc_dict.keys())
         values = list(self.toc_dict.values())
@@ -116,21 +176,17 @@ class NamuCrawler(object):
             index = keys.index(target_key)
             if index < len(keys) - 1:
                 return keys[index + 1], values[index + 1]
-            else:
-                return {keys[index]: values[index]}  # 마지막 키인 경우 다음 아이템이 없음
-        else:
-            return None
+            return {keys[index]: values[index]}  # 마지막 키인 경우 다음 아이템이 없음
+        return None
 
     def get_content_heading(self, heading_idx):
         """헤드 값의 컨텐츠 내용을 반환"""
         start_tag = self.toc_dict.get(heading_idx)[1]
         try:
-            end_tag = self.get_item_and_next(heading_idx)[1][1] 
-        except(KeyError): #마지막 헤딩(보통 각주영역)의 경우엔 직접 로딩
-            if self.toc_dict.get(heading_idx)[1] == None: # 각주가 없는 문서
+            end_tag = self.get_next_item(heading_idx)[1][1] 
+        except KeyError: #마지막 헤딩(보통 각주영역)의 경우엔 직접 로딩
+            if self.toc_dict.get(heading_idx)[1] is None: # 각주가 없는 문서
                 return (self.toc_dict.get(heading_idx)[0], None)
-            else:
-                return (self.toc_dict.get(heading_idx)[0], self.toc_dict.get(heading_idx)[1].get_text())
-        content = self.get_content_between_tags(head = self.toc_dict.get(heading_idx)[0], start_tag= start_tag, end_tag= end_tag)
-
-        return content
+            return (self.toc_dict.get(heading_idx)[0], [self.toc_dict.get(heading_idx)[1].get_text()])
+        return self.get_content_between_tags(head = self.toc_dict.get(heading_idx)[0], start_tag= start_tag, end_tag= end_tag)
+    
--- namu_loader.py
+++ namu_loader.py
@@ -1,9 +1,10 @@
 import os
-from namu_crawler import namuCrawler
+from namu_crawler import NamuCrawler
 from langchain_core.documents import Document
 from langchain_community.document_loaders.base import BaseLoader
-from typing import Iterator
+from typing import List
 import time
+BASE_URL = "https://namu.wiki"
 
 class NamuLoader(BaseLoader):
     """Load NamuWiki pages using `namuClawler`."""
@@ -11,54 +12,95 @@ class NamuLoader(BaseLoader):
         self.url = url
         self.max_hop = max_hop
         self.verbose = verbose
-        self.base_nc = namuCrawler(url = self.url, hop = 0)
+        self.base_nc = NamuCrawler(url = self.url, hop = 0)
         self.base_nc.construct_toc()
         if (self.verbose) == True:
             self.base_nc.print_toc()
 
-    def get_total_content(self, parent_item, sub_url, hop):
-        sub_nc = namuCrawler(url = sub_url, hop = hop)
-        sub_nc.construct_toc()
-        # print(sub_nc.get_doc_title(), parent_item, sub_nc.hop, max_hop)
-        to_return = ""
+    def make_sub_nc(self, parent_nc, sub_url, hop, parent_meta):
         start = time.time()
-        for k, v in sub_nc.toc_dict.items():
-            cur_toc_item, content = sub_nc.get_content_heading(k)
-            
-            if type(content) == str and f'/w/' in content: # content가 링크 대체이면서 
-                if sub_nc.hop < self.max_hop: #현재 문서의 hop이 max_hop보다 적거나 같으면 더 들어가기
-                    content = self.get_total_content(parent_item = parent_item, sub_url = base_url + content, hop = sub_nc.hop + 1)
-                else: # max_hop과 같으면 그냥 링크로 대체한다고만 써주기
-                    content = f"{cur_toc_item[1]}: 다음 문서로 대체 설명: {base_url + content}"
-            else: # 일반 설명은 {현재 목차 : 설명} 꼴로 구성
-                content = f'{cur_toc_item[1]}: {" ".join(content) if type(content) == list else ""}'
-            
-            to_return = to_return + "\n" + content + "\n"
-
-        # if self.verbose:
-        print(f"Sub document {sub_nc.get_doc_title()} done (hop : {sub_nc.hop}/{self.max_hop} \t elapsed_time: {round(time.time() - start, 1)} seconds)")
-        return to_return
-
-    def get_a_content(self, header):
-        """하나의 header의 본문 내용 가져오기"""
-        cur_toc_item, content = self.base_nc.get_content_heading(header)
-        if content == None:
-            return (cur_toc_item, None)
-        elif type(content) == str and '/w/' in content: # content가 링크 대체라면
-            return (cur_toc_item, self.get_total_content(parent_item = self.base_nc.toc_dict.get(header)[0], sub_url = base_url + content, hop = self.base_nc.hop + 1))
-        else:
-            return (cur_toc_item, " ".join(content))
         
-    def lazy_load(self) -> Iterator[Document]:
-        """Iterate over content items to load Documents"""
-        for s, header in self.base_nc.toc_dict.items():
-            ((index, toc_item), cur_content) = self.get_a_content(s)
-            cur_metadata = {
-                "index" : index,
-                "toc_item" : toc_item
-            }
+        sub_nc = NamuCrawler(url = sub_url, hop = hop)
+        
+        if self.verbose:
+            formatted_str = f"{'  '*sub_nc.hop}Sub Doc {sub_nc.get_doc_title()}"
+            print(f"{formatted_str:<50} Start (hop : {sub_nc.hop}/{self.max_hop})")
+        sub_nc.construct_toc()
+        to_return_docs = []
+        for s, header in sub_nc.toc_dict.items():
+            to_return_docs += self.get_docs(sub_nc, s, parent_meta)
+        
+        if self.verbose:
+            print(f"{formatted_str:<50} Done! (hop : {sub_nc.hop}/{self.max_hop}, elapsed_time: {round(time.time() - start, 1)} seconds)")
 
-            if self.verbose == True:
-                print(">> ", s, header[0][1])
-                print("\t", cur_content)
-            yield Document(page_content= cur_content, metadata = cur_metadata)
+        return to_return_docs
+    
+    def get_docs(self, nc, s, parent_meta = None):
+        """본문 내용 가져오기"""
+        cur_toc_item, content = nc.get_content_heading(s)
+        if parent_meta == None:
+            parent_page_index = ""
+            parent_page_toc_item = ""
+            abs_page_toc_items = (nc.get_ancestor_items(toc_index = cur_toc_item[0]) + "/" ) + cur_toc_item[1] if nc.get_ancestor_items(toc_index = cur_toc_item[0]) != '' else '' + cur_toc_item[1]
+            
+        else:
+            parent_page_index = parent_meta['index']
+            parent_page_toc_item = parent_meta['toc_item']
+            abs_page_toc_items = (
+                parent_meta['abs_page_toc_item'] 
+                + "//" 
+                + ((nc.get_ancestor_items(toc_index = cur_toc_item[0]) + "/" ) if nc.get_ancestor_items(toc_index = cur_toc_item[0]) != '' else '')
+                + cur_toc_item[1]
+            )
+            
+        meta_data = {
+            "base_page_url" : parent_meta['base_page_url'] if parent_meta else nc.url,
+            "parent_page_url" : parent_meta['current_page_url'] if parent_meta else None,
+            "current_page_url" : nc.url,
+            "page_hop" : nc.hop,
+
+            "parent_page_index" : parent_page_index,
+            "parent_page_toc_item" : parent_page_toc_item,
+            "abs_page_toc_item": abs_page_toc_items,
+
+            "index" : cur_toc_item[0],
+            "toc_item" : cur_toc_item[1],
+            "ancestor_toc_item" : nc.get_ancestor_items(toc_index = cur_toc_item[0])
+        }
+
+        if content == None:
+            return [Document(page_content = "", metadata = meta_data)]
+
+        elif type(content) == str and '/w/' in content: 
+            # content가 링크 대체라면 
+            if nc.hop + 1 <= self.max_hop: # max_hop보다 아래라면 링크로 들어가 전체 다 가져오기
+                sub_docs = self.make_sub_nc(
+                    parent_nc = nc, 
+                    sub_url = BASE_URL + content, 
+                    hop = nc.hop + 1,
+                    parent_meta = meta_data
+                )
+                return sub_docs
+            else: # 아니라면 설명으로 대체
+                return  [Document(page_content = f"다음 문서로 대체 설명: {BASE_URL + content}", metadata = meta_data)]
+        else:
+            return [Document(page_content = " ".join(content), metadata = meta_data)]
+
+    def lazy_load(self) -> List[Document]:
+        """Iterate over content items to load Documents"""
+        
+        docs = []
+        start = time.time()
+
+        if self.verbose:
+            print(f"Main Doc {self.base_nc.get_doc_title():<50} Start (hop : {self.base_nc.hop}/{self.max_hop})")
+        
+
+        for s in self.base_nc.toc_dict.keys():
+            cur_docs = self.get_docs(self.base_nc, s)
+            docs += cur_docs
+        
+        if self.verbose:
+            print(f"Main Doc {self.base_nc.get_doc_title():<50} Done! (hop : {self.base_nc.hop}/{self.max_hop}, elapsed_time: {round(time.time() - start, 1)} seconds)")
+
+        return docs
